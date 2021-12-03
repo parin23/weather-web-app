d2f9b4755314a78a4e712b6284127ccbff593ebd
praguru14 <48213609+praguru14@users.noreply.github.com>
2021-12-03 09:30:03 +0530

    Added: Clicking on temp changes it to Fahrenheit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const cityInput = document.querySelector("#city-input");
 
 const weather = {};
 weather.temperature = {
-    unit:"celsius"
+    unit: "celsius"
 }
 
 let geolocation = false;
@@ -24,16 +24,14 @@ const KEY = '82005d27a116c2880c8f0fcb866998a0';
 today = new Date();
 dateElement.innerHTML = `<p>${today.toDateString() }</p>`;
 
-function checkWeather(){
-    if(!geolocation){
+function checkWeather() {
+    if (!geolocation) {
         getWeather(null, null);
-    }
-    else{
+    } else {
         // Check if Browser Supports Location
-        if('geolocation' in navigator){
+        if ('geolocation' in navigator) {
             navigator.geolocation.getCurrentPosition(setPosition, showError);
-        }
-        else{
+        } else {
             notificationElement.style.display = "block";
             notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
         }
@@ -42,70 +40,74 @@ function checkWeather(){
 
 // Set the Location
 
-function setPosition (position){
+function setPosition(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-    getWeather(latitude,longitude);
+    getWeather(latitude, longitude);
 }
 
 // Show error
 
-function showError(error){
+function showError(error) {
     notificationElement.style.display = "block";
-    if(error.message != null){
+    if (error.message != null) {
         notificationElement.innerHTML = `<p>${error.message}</p>`;
-    }
-    else{
+    } else {
         notificationElement.innerHTML = `<p>${error}</p>`;
     }
 }
 
 // GET Weather from API
 
-function getWeather(latitude,longitude){
+function getWeather(latitude, longitude) {
 
     let city = cityInput.value;
 
     let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}`;
-        if (geolocation){
-             api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}`;
-        }
+    if (geolocation) {
+        api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}`;
+    }
     fetch(api)
-    .then(function(response){
-        let data = response.json();
-        return data;
-    })
-    .then(function(data){
-        if(data.cod == 200){
-            notificationElement.style.display = "";
-            weather.temperature.value = data.main.temp;
-            weather.description = data.weather[0].description;
-            weather.iconID= data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
-            displayWeather();
-        }
-        else{
-            showError("Invalid City");
-        }
-    })
+        .then(function(response) {
+            let data = response.json();
+            return data;
+        })
+        .then(function(data) {
+            if (data.cod == 200) {
+                notificationElement.style.display = "";
+                weather.temperature.value = data.main.temp;
+                weather.description = data.weather[0].description;
+                weather.iconID = data.weather[0].icon;
+                weather.city = data.name;
+                weather.country = data.sys.country;
+                displayWeather();
+            } else {
+                showError("Invalid City");
+            }
+        })
 }
 
-function displayWeather(){
+function displayWeather() {
+    console.log(weather.temperature.value);
     iconElement.innerHTML = `<img src="icons/${weather.iconID}.png"/>`;
     tempElement.innerHTML = `${Math.floor(weather.temperature.value - KELVIN)}° <span>C</span>`;
     desElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
 
+function cToF() {
+    const fah = ((weather.temperature.value - KELVIN) * 9 / 5) + 32;
+    tempElement.innerHTML = `${Math.floor(fah)}° <span>F</span>`;
+}
+
 // WHEN THE USER CLICKS ON THE BUENOS AIRES BUTTON ELEMENT
-inputWeatherButton.addEventListener("click", function(){
+inputWeatherButton.addEventListener("click", function() {
     geolocation = false;
     checkWeather();
 });
 
 // WHEN THE USER CLICKS ON THE CURRENT LOCATION BUTTON ELEMENT
-currentWeatherButton.addEventListener("click", function(){
+currentWeatherButton.addEventListener("click", function() {
     geolocation = true;
     checkWeather();
 });
